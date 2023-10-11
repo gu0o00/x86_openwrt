682634570e4f08cea2897b823b672efcc2f7fb63
gu0o00 <g975291783@163.com>
2023-10-11 09:42:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,7 @@
 #
 
 rm -rf feeds/luci/themes/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+rm -rf package/luci-app-openclash && git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
