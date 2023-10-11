aaecc432ce6954cc417e51f1f58776379b409f7f
guojian <guojian@nsfocus.com>
2023-10-11 11:02:49 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,22 @@
 #
 
 rm -rf feeds/luci/themes/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-rm -rf package/luci-app-openclash && git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.0.0.100/g' package/base-files/files/bin/config_generate
+
+# clone openclash
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+# 回退到主项目目录
+cd ../..
